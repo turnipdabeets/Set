c937ed9edadd97de66c2f343090c2b68bbcc9e03
Anna Garcia <annaegarcia@gmail.com>
2018-05-25 09:32:56 -0400

    refactor style button by card

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -18,36 +18,44 @@ class ViewController: UIViewController {
     }
     @IBOutlet var cardButtons: [UIButton]! {
         didSet {
-            
-            let colorGroup = [#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1), #colorLiteral(red: 0.5791940689, green: 0.1280144453, blue: 0.5726861358, alpha: 1), #colorLiteral(red: 0, green: 0.5628422499, blue: 0.3188166618, alpha: 1)]
-            let symbolGroup = ["\u{25B2}", "\u{25AE}", "\u{25CF}"] // triangle, rectangle, circle
-            
             for button in cardButtons {
                 if let cardIndex = cardButtons.index(of: button){
                     let card = game.cards[cardIndex]
-                    let cardColor = colorGroup[card.color]
-                    var attribute: [NSAttributedStringKey: Any] = [:]
-                        if card.shading == 0 {
-                            // striped
-                            attribute[.foregroundColor] = cardColor.withAlphaComponent(0.15)
-                        } else if card.shading == 1 {
-                            // solid
-                            attribute[.foregroundColor] = cardColor.withAlphaComponent(1.0)
-                        } else {
-                            // open
-                            attribute[.strokeColor] = cardColor
-                            attribute[.strokeWidth] = 5.0
-                        }
-                    var shape = ""
-                    for _ in 0...card.number {
-                        shape += symbolGroup[card.symbol]
-                    }
-                    let attributedString = NSAttributedString(string: shape, attributes: attribute)
-                    button.setAttributedTitle(attributedString, for: .normal)
+                    style(a: button, by: card)
                 }
             }
         }
     }
+    
+    private func style(a button: UIButton, by card: Card) {
+        // with color
+        let colorGroup = [#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1), #colorLiteral(red: 0.5791940689, green: 0.1280144453, blue: 0.5726861358, alpha: 1), #colorLiteral(red: 0, green: 0.5628422499, blue: 0.3188166618, alpha: 1)]
+        let cardColor = colorGroup[card.color]
+        // with symbol
+        let symbolGroup = ["\u{25B2}", "\u{25AE}", "\u{25CF}"] // triangle, rectangle, circle
+        // with shading
+        var attribute: [NSAttributedStringKey: Any] = [:]
+        if card.shading == 0 {
+            // striped
+            attribute[.foregroundColor] = cardColor.withAlphaComponent(0.15)
+        } else if card.shading == 1 {
+            // solid
+            attribute[.foregroundColor] = cardColor.withAlphaComponent(1.0)
+        } else {
+            // open
+            attribute[.strokeColor] = cardColor
+            attribute[.strokeWidth] = 5.0
+        }
+        // with number
+        var shape = ""
+        for _ in 0...card.number {
+            shape += symbolGroup[card.symbol]
+        }
+        
+        let attributedString = NSAttributedString(string: shape, attributes: attribute)
+        button.setAttributedTitle(attributedString, for: .normal)
+    }
+    
     
     
 }
