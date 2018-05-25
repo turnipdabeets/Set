71dbb0132a229a475f8ad0d790ed1d901ee4935b
Anna Garcia <annaegarcia@gmail.com>
2018-05-24 21:45:20 -0400

    fully connect ui styles to model

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -15,32 +15,37 @@ class ViewController: UIViewController {
         if let cardIndex = cardButtons.index(of: sender) {
             game.chooseCard(at: cardIndex)
         }
-        print("card selected, \(sender)")
     }
     @IBOutlet var cardButtons: [UIButton]! {
         didSet {
             
-            var colorGroup = [#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1), #colorLiteral(red: 0.5791940689, green: 0.1280144453, blue: 0.5726861358, alpha: 1), #colorLiteral(red: 0, green: 0.5628422499, blue: 0.3188166618, alpha: 1)]
+            let colorGroup = [#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1), #colorLiteral(red: 0.5791940689, green: 0.1280144453, blue: 0.5726861358, alpha: 1), #colorLiteral(red: 0, green: 0.5628422499, blue: 0.3188166618, alpha: 1)]
+            let symbolGroup = ["\u{25B2}", "\u{25AE}", "\u{25CF}"] // triangle, rectangle, circle
             
             for button in cardButtons {
                 if let cardIndex = cardButtons.index(of: button){
-//                    print("button \(button) \(cardIndex)")
-//                    print("matching card \(game.cards[cardIndex])")
                     let card = game.cards[cardIndex]
-                    print(card.color)
-                    let attribute: [NSAttributedStringKey: Any] = [
-                        .strokeColor: colorGroup[card.color],
-                        .strokeWidth: 5.0
-                    ]
-                    let attributedString = NSAttributedString(string: "hello", attributes: attribute)
+                    let cardColor = colorGroup[card.color]
+                    var attribute: [NSAttributedStringKey: Any] = [:]
+                        if card.shading == 0 {
+                            // striped
+                            attribute[.foregroundColor] = cardColor.withAlphaComponent(0.15)
+                        } else if card.shading == 1 {
+                            // solid
+                            attribute[.foregroundColor] = cardColor.withAlphaComponent(1.0)
+                        } else {
+                            // open
+                            attribute[.strokeColor] = cardColor
+                            attribute[.strokeWidth] = 5.0
+                        }
+                    var shape = ""
+                    for _ in 0...card.number {
+                        shape += symbolGroup[card.symbol]
+                    }
+                    let attributedString = NSAttributedString(string: shape, attributes: attribute)
                     button.setAttributedTitle(attributedString, for: .normal)
-//                    var a = button.attributedTitle(for: UIControlState.normal)
-//                    print("hm", a)
-
                 }
-                
             }
-//            print("cards set \(self) \(cardButtons)")
         }
     }
     
